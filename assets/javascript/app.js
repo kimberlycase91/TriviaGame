d49246370f144817b8f0af0945e8822550af07d8
kimberlycase91 <kimberlycase91@gmail.com>
2020-01-06 22:12:28 -0700

    outlined basic html with id and class tags

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//global variables 
+var questions = [] //holds questions and answer options as objects;
+var correct = 0; //keeps track of correct answers
+var incorrect = 0; //keeps track of incorrect answers
+var unanswered = 0; //keeps track  of unanswered questions
+
+//when start button is clicked 
+document.on("click")
+//loop through each question in array
+
+//display question for set amount of time
+
+//if time runs out, display correct answer and image and increment unanswered variable by 1
+
+//else if user selects correct answer, display correct answer and imagne and increment correct variable by 1
+
+//else if user selects an incorrect anwer, display correct answer and image and increment incorrect variable by 1
+
+//after each question in the array has been answered, change display to show "Here's how you did!", display correct, incorrect, and unanswered, and a start over button
