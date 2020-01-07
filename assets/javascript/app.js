b07f52e1258702ea51fa11462fe628d19dba0415
kimberlycase91 <kimberlycase91@gmail.com>
2020-01-06 22:58:44 -0700

    set up start button to show quiz when clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,16 +4,26 @@ var correct = 0; //keeps track of correct answers
 var incorrect = 0; //keeps track of incorrect answers
 var unanswered = 0; //keeps track  of unanswered questions
 
-//when start button is clicked 
-document.on("click")
-//loop through each question in array
+//only display start button...
+$(document).ready(function () {
+    $("#quizDisplay").hide();
 
-//display question for set amount of time
+    //when start button is clicked, hide start button and show quiz content
+    $("#startBtn").click(function () {
+        $("#quizDisplay").toggle();
+        $("#startBtn").hide();
 
-//if time runs out, display correct answer and image and increment unanswered variable by 1
+        //loop through each question in array
+        for (var i=0; i < questions.length; i++) {
+        //display question for set amount of time
+       
+        //if time runs out, display correct answer and image and increment unanswered variable by 1
+        
+        //else if user selects correct answer, display correct answer and imagne and increment correct variable by 1
 
-//else if user selects correct answer, display correct answer and imagne and increment correct variable by 1
-
-//else if user selects an incorrect anwer, display correct answer and image and increment incorrect variable by 1
-
-//after each question in the array has been answered, change display to show "Here's how you did!", display correct, incorrect, and unanswered, and a start over button
+        //else if user selects an incorrect anwer, display correct answer and image and increment incorrect variable by 1
+    
+        //after each question in the array has been answered, change display to show "Here's how you did!", display correct, incorrect, and unanswered, and a start over button
+    }
+    });
+})
